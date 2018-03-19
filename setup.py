6ac7be5d223a9c9d22df25588ae47a27acce44d8
Francesco "c1cc10" Rana <6458823+c1cc10@users.noreply.github.com>
2018-03-19 15:14:05 +0100

    windows version
    to be used along with winpython

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(name='barcode-clock',
       version='0.1',
       description='clock whose output is barcode',
+      long_description=""" Desktop Clock that outputs date time in barcode format. It can be easily read by one barcode scanner for quickly insert. As barcode format we use 128 """,
       url='https://github.com/c1cc10/barcode-clock',
       author='Francesco Rana',
       author_email='rana@isolved.it',
       license='GPL',
       packages=['barCodeClock'],
+      py_modules=["code128_test"],
       install_requires=[
           'PIL',
           'pygame',
